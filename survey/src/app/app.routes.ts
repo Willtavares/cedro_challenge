ece28105921293ff9b0c143d10e5e91db8400224
William Tavares <willtavares@outlook.com>
2024-08-14 21:47:28 -0300

    Criação do DB, app e arquitetura

---
+++ survey/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { IntroComponent } from './surveys/page/intro/intro.component';
+
+export const routes: Routes = [{ path: '', component: IntroComponent }];
